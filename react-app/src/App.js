7bbe1976eb61633ef0ee3121faabf56262225f12
utkamag <92789705+utkamag@users.noreply.github.com>
2022-06-29 12:44:10 +0400

    update code

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.scss';
+
+function App() {
+    return (
+            <div className="header">
+                <div className="header__inner">123</div>
+            </div>
+    );
+}
+
+export default App;
