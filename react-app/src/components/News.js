0e7c86b6d8668c366401e236a7c4445300fcde32
utkamag <92789705+utkamag@users.noreply.github.com>
2022-07-04 15:20:52 +0400

    update code
    Фильтрация массива

--- react-app/src/components/News.js
+++ react-app/src/components/News.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NEWS_DATA from "../assets/JSON/package.json"
 import {useDispatch} from "react-redux";
 
 const News = () => {
 
+    const data = NEWS_DATA
 
+    //Стейт массива и фильтрации данных
+
+    const [newsList, setNewsList] = useState(data)
+    const [search, setSearch] = useState("")
 
     //Redux
 
     let dispatch = useDispatch()
 
-    // При клике на блок новостей включаем popup
+    //При клике на блок новостей включаем popup
     //Передаем данные с массива в store, для последующей передачи в popupNews
 
     const popupOn = (e) => {
@@ -23,17 +28,19 @@ const News = () => {
 
     return (
         <div className="wrapper">
+            <input className="search__input" type="text" placeholder="Поиск новостей" value={search}
+                   onChange={(e) => setSearch(e.target.value.toLowerCase())}/>
             <div className="news">
-                {NEWS_DATA.map((data) => {
+                {newsList.filter((list) => list.news_title.toLowerCase().includes(search)).map((data) => {
                     return (
                         <div className="news__widget" onClick={popupOn}>
-                            <div className="news__widget-title">
+                            <div className="news__widget-title" key={newsList.id}>
                                 {data.news_title}
                             </div>
-                            <div className="news__widget-text">
+                            <div className="news__widget-text" key={newsList.id}>
                                 {data.news_text}
                             </div>
-                            <div className="news__widget-date">
+                            <div className="news__widget-date" key={newsList.id}>
                                 {data.date}
                             </div>
                         </div>
